05781667a582ba2564e518bc6e11e510c7b7cc6c
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-23 18:56:23 +0530

    Update database.py

--- AloneRobot/utils/database.py
+++ AloneRobot/utils/database.py
@@ -67,7 +67,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from Spy.core.userbot import assistants
+    from AloneRobot.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -81,7 +81,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from Spy.core.userbot import assistants
+    from AloneRobot.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -108,7 +108,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from Spy.core.userbot import assistants
+    from AloneRobot.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -121,7 +121,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from Spy.core.userbot import assistants
+    from AloneRobot.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -644,4 +644,5 @@ async def remove_banned_user(user_id: int):
     if not is_gbanned:
         return
     return await blockeddb.delete_one({"user_id": user_id})
+
 
