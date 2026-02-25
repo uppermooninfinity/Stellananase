3795c04f3804ef9dce5be40528867cbf0acf7f26
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-25 23:15:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,70 +7,70 @@ from pyrogram import filters
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(os.getenv("API_ID", ""))
 
-API_HASH = getenv("API_HASH", "")
+API_HASH = os.getenv("API_HASH", "")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 54000))
+DURATION_LIMIT_MIN = int(os.getenv("DURATION_LIMIT", 54000))
 
 SONG_DOWNLOAD_DURATION = int(
-    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "54000")
+    os.getenv("SONG_DOWNLOAD_DURATION_LIMIT", "54000")
 )
 
-LOGGER_ID = int(getenv("LOGGER_ID", ""))
+LOGGER_ID = int(os.getenv("LOGGER_ID", ""))
 
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(os.getenv("OWNER_ID", ""))
 
-BOT_USERNAME = getenv("BOT_USERNAME" , "")
+BOT_USERNAME = os.getenv("BOT_USERNAME" , "")
 
-COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
+COMMAND_HANDLER = os.getenv("COMMAND_HANDLER", "! / .").split()
 
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
+UPSTREAM_REPO = os.getenv(
     "UPSTREAM_REPO",
     "",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "https://github.com/uppermooninfinity/Stellananase")
-GIT_TOKEN = getenv(
+UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "https://github.com/uppermooninfinity/Stellananase")
+GIT_TOKEN = os.getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dark_musictm")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/snowy_hometown")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/dark_musictm")
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/snowy_hometown")
 
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
-AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
+AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_SUGGESTION_MODE = os.getenv("AUTO_SUGGESTION_MODE", "True")
 AUTO_SUGGESTION_TIME = int(
-    getenv("AUTO_SUGGESTION_TIME", "500"))
+    os.getenv("AUTO_SUGGESTION_TIME", "500"))
 
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "6fb7e1766693439b86ec57e3deb3c36f")
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "da3f94c6a68d49f6b64a7216ec9eb905")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", "6fb7e1766693439b86ec57e3deb3c36f")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", "da3f94c6a68d49f6b64a7216ec9eb905")
 
 
 
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_FETCH_LIMIT = int(os.getenv("PLAYLIST_FETCH_LIMIT", 25))
 
 CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "5"))
+    os.getenv("CLEANMODE_MINS", "5"))
 
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 21474836480))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 21474836480))
+TG_AUDIO_FILESIZE_LIMIT = int(os.getenv("TG_AUDIO_FILESIZE_LIMIT", 21474836480))
+TG_VIDEO_FILESIZE_LIMIT = int(os.getenv("TG_VIDEO_FILESIZE_LIMIT", 21474836480))
 
 
 
 # Get your pyrogram v2 session from @Shsusu_bot on Telegram
-STRING1 = getenv("STRING_SESSION", "")
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
+STRING1 = os.getenv("STRING_SESSION", "")
+STRING2 = os.getenv("STRING_SESSION2", None)
+STRING3 = os.getenv("STRING_SESSION3", None)
+STRING4 = os.getenv("STRING_SESSION4", None)
+STRING5 = os.getenv("STRING_SESSION5", None)
 
 
 BANNED_USERS = filters.user()
@@ -85,10 +85,10 @@ clean = {}
 
 autoclean = []
 
-START_IMG_URL = getenv(
+START_IMG_URL = os.getenv(
     "START_IMG_URL", "https://files.catbox.moe/k8ix72.jpg"
 )
-PING_IMG_URL = getenv(
+PING_IMG_URL = os.getenv(
     "PING_IMG_URL", "https://files.catbox.moe/k8ix72.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/k8ix72.jpg"
