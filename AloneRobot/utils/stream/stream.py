a888ebdbbd45aec1500cb078574d32fce27d2edc
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-25 16:48:31 +0530

    Update and rename stream (1).py to stream.py

--- AloneRobot/utils/stream/stream.py
+++ AloneRobot/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from Spy import Carbon, YouTube, app
-from Spy.core.call import Sagar
-from Spy.misc import db
-from Spy.utils.database import add_active_video_chat, is_active_chat
-from Spy.utils.exceptions import AssistantErr
-from Spy.utils.inline import aq_markup, close_markup, stream_markup
-from Spy.utils.pastebin import SagarBin
-from Spy.utils.stream.queue import put_queue, put_queue_index
-from Spy.utils.thumbnails import get_thumb
+from AloneRobot import Carbon, YouTube, app
+from AloneRobot.core.call import Sagar
+from AloneRobot.misc import db
+from AloneRobot.utils.database import add_active_video_chat, is_active_chat
+from AloneRobot.utils.exceptions import AssistantErr
+from AloneRobot.utils.inline import aq_markup, close_markup, stream_markup
+from AloneRobot.utils.pastebin import SagarBin
+from AloneRobot.utils.stream.queue import put_queue, put_queue_index
+from AloneRobot.utils.thumbnails import get_thumb
 
 
 async def stream(
